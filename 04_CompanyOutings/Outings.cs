255654f242fb3d56d2d4ce64954db18d4a0b607d
Andrew Sitter <sittera16@gmail.com>
2021-12-20 14:59:11 -0500

    Completed Outings Class and Rep

---
+++ 04_CompanyOutings/Outings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CompanyOutings
+{
+    public enum EventType
+    {
+        Golf = 1,
+        Bowling,
+        AmusementPark,
+        Concert,
+    }
+
+    public class Outings
+    {
+        public Outings() { }
+        public Outings (EventType eventType, int attendeeCount, DateTime date, int eventCost) 
+        {
+            EventType = eventType;
+            AttendeeCount = attendeeCount;
+            Date = date;
+            EventCost = eventCost;
+        }
+
+        public EventType EventType { get; set; }
+        public int AttendeeCount { get; set; }
+        public DateTime Date { get; set; }
+        public int EventCost { get; set; }
+        public int CostPerPerson
+        {
+            get
+            {
+                int quotient = EventCost / AttendeeCount;
+                return quotient;
+            }
+        }
+    }
+}
---
+++ 04_CompanyOutings/OutingsRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CompanyOutings
+{
+    public class OutingsRepository
+    {
+        public List<Outings> _outings = new List<Outings>();
+
+        public bool AddOutingtoDirectory(Outings outing)
+        {
+            int startingCount = _outings.Count;
+            _outings.Add(outing);
+
+            bool wasAdded = (_outings.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+
+        public List<Outings> GetAllOutings()
+        {
+            return _outings;
+        }
+
+        public int GetTotalOutingsCost()
+        {
+            int totalCost = 0;
+            List<Outings> outings = GetAllOutings();
+            foreach(Outings outing in outings)
+            {
+                totalCost+=outing.EventCost;
+            }
+            return totalCost;
+        }
+        public int GetTotalCostByOutingType(EventType eventType)
+        {
+            int totalCost = 0;
+            List<Outings> outings = GetAllOutings();
+            foreach (Outings outing in outings)
+            {
+                if (outing.EventType == eventType)
+                {
+                    totalCost += outing.EventCost;
+                }
+            }
+            return totalCost;
+        }
+    }
+}
